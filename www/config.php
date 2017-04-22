125dd80e263bf1ecc5f041b5ccdc6a29badecffc
VictorAng <spa995@uowmail.edu.au>
2017-04-22 19:56:35 +1000

    Added mysql config

---
+++ www/config.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_HOST = 'iot.csosz45qwa0w.ap-southeast-2.rds.amazonaws.com';
+const DB_USER = 'root';
+const DB_PASS = 'testavocado';
+const DB_NAME = 'analytics';
--- www/index.php
+++ www/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Test deployment!";
+require_once 'config.php';
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo("success");
+}
+echo "Connected successfully";
+
+echo "Hello test deployment!";
