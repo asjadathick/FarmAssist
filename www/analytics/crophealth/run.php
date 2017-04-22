c703321948af3dd0bfafac7e30e0422654365cdc
Asjad <asjadathick@gmail.com>
2017-04-22 19:47:33 +1000

    Base

--- www/analytics/crophealth/run.php
+++ www/analytics/crophealth/run.php
@@ -1,1 +1,17 @@
 <?php
+/*
+ * crophealth/run.php
+ * athick@asjad.io
+ * Writes daily health score for crop to table
+ */
+
+
+$crophealthconfig = array(
+    "name" => "crophealth"
+);
+
+//TODO: get last run time for analytic
+
+//TODO: analyse only data recieved after last run time (filtered for each deployed sensor on it's crop type
+
+echo "Crop health analytic successfully ran.";
